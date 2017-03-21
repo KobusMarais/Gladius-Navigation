2ea3f6ddf73b569ee19bc6c4eadfe89c090db8fe
vossca <mgerber0608@gmail.com>
2017-03-21 16:26:41 +0200

    Add files via upload

---
+++ NavigationExceptions.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.lang.Object;
+import java.lang.Throwable;
+
+public class NavigationExceptions extends Exception {
+	public NavigationExceptions() {}
+
+	public NavigationExceptions(String mesg) {
+		super(mesg);
+	}
+
+	public NavigationExceptions(Throwable reason) {
+		super(reason);
+	}
+
+	public NavigationExceptions(String mesg, Throwable reason) {
+		super(mesg,reason);
+	}
+
+}
