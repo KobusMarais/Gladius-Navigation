66ad41bd421c2c46627a1bed48e48d4c2941b520
Victor <victor@Drs-MacBook-Pro.Home>
2017-03-22 21:45:22 +0200

    added GPSObject and Coords test files

---
+++ GPSObjectTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by victor on 2017/03/22.
+ */
+class GPSObjectTest {
+    @Test
+    void getCoords() {
+        assertNotNull(getCoords("roo"));
+    }
+
+}
