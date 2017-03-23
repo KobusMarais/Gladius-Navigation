fb6e431bb458782b9da1fefd1e5d97c5b7b69d4e
vossca <mgerber0608@gmail.com>
2017-03-23 13:43:37 +0200

    Update FailedValidation.java

--- FailedValidation.java
+++ FailedValidation.java
@@ -21,4 +21,7 @@ public class FailedValidation extends NavigationExceptions {
 
 	protected String defaultMessage = new String("User did not enter the correct credentials or validation failed elsewhere.");
 	protected Throwable defaultCause = new Throwable(defaultMessage);
+	
+	/*Basic Exceptions class which will be raised in the event of user credentials not validating, this will
+	occur when we try to fetch preferences from the database but we cannot find the user in the database*/
 }
