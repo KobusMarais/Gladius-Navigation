010409b5316134b124484dd0f034c7ed9c9c8f70
vossca <mgerber0608@gmail.com>
2017-03-23 13:44:56 +0200

    Update IncompleteInformation.java

--- IncompleteInformation.java
+++ IncompleteInformation.java
@@ -21,4 +21,7 @@ public class IncompleteInformation extends NavigationExceptions {
 
 	protected String defaultMessage = new String("User did not provide enough information to find a route.");
 	protected Throwable defaultCause = new Throwable(defaultMessage);
+	
+	/* Basic exception that will be raised when nformation that is essential to creating a new route is missing, or enough 
+	information for route manipulation was not provided. */
 }
