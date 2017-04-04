0242c03b167a78626135b8c276903e7a311c5121
vossca <mgerber0608@gmail.com>
2017-04-04 18:41:12 +0200

    Update Location.java

--- Navigation/src/main/java/com/gladius/navup/navigation/Location.java
+++ Navigation/src/main/java/com/gladius/navup/navigation/Location.java
@@ -11,6 +11,12 @@ public class Location {
     private String locationID;
     private GPSObject coordinates;
 
+    /**
+    *   Constructor that will create a new location object from a GPS object and assign it a name and id
+    *   @param  n   String that will be the name for the specific location example: HB 4-14
+    *   @param id   String that will be a unique identifier for the object
+    *   @param obj  GPSObject received from GIS module containing latitude and longitude of location
+    */
     public Location(String n, String id, GPSObject obj)
     {
         this.name = n;
