eb0b55d6895ae9a27881fd36f381aaf8d6815c42
Nkosinathi Mothoa <nmothoa360@gmail.com>
2017-04-04 17:31:57 +0200

    Documentation added -- JavaDoc

--- Navigation/src/main/java/com/gladius/navup/navigation/Route.java
+++ Navigation/src/main/java/com/gladius/navup/navigation/Route.java
@@ -3,71 +3,78 @@ import java.util.*;
 
 public class Route
 {
-		private String name;
-		private Locations path;
-	
-		public Route()
-		{
-				name = "";
-				path = null;
-		}
-	
-		public Route(String V, Route M)
-		{
-			
-		}
-	
-		public Route(String rN)
-		{
-				name = rN;
-				path = new Locations();
-		}
-		
-		public Route(String rN, Locations clone)
-		{
-				name = rN;
-				path = new Locations();
-				path = clone;
-		}
+	private String name;
+	private Locations path;
 
-		public Route(Route rt)
-		{
-			this.name = rt.name;
-			this.path = rt.path;
-		}
+	public Route(String V, Route M)
+	{
+
+	}
+
+	public Route()
+	{
+		name = "";
+		path = null;
+	}
+
+	public Route(String rN)
+	{
+		name = rN;
+		path = new Locations();
+	}
+		
+	public Route(String rN, Locations clone)
+	{
+		name = rN;
+		path = new Locations();
+		path = clone;
+	}
+
+	public Route(Route rt)
+	{
+		this.name = rt.name;
+		this.path = rt.path;
+	}
 	
-		public String getName()
+	public String getName()
 		{
 				return name;	
 		}
-		
-		public Locations calculateRoute(Locations routeArray)
-		{
-				Boolean locationObjectsAreValid = true;
-		
-				if(locationObjectsAreValid)
-				{
-					Locations wayPoints = new Locations();
-						//ArrayList<Location> wayPoints = new ArrayList<Location>();
-						int i = 0;
-						while(i < routeArray.size())
-						{
-							wayPoints.addLocation(routeArray.get(i));
-						}
-				
-						System.out.println("Calculating route...");
-						System.out.println("Route calculation successful!\n");
-						return wayPoints;
-				}
-				else
-				{
-						System.out.println("Location Objects are invalid.\n");
-				}
-				
-				return null;
-		}
 
-		public Locations getLocations()
+	/**
+	 *	This method
+	 *
+	 * @param routeArray Locations object encapsulating the start and end Locations.
+	 * @return a Locations object encapsulating the wayPoints to reach a given destination from a given starting Location.
+	 */
+	public Locations calculateRoute(Locations routeArray)
+	{
+		Boolean locationObjectsAreValid = true;
+		
+		if(locationObjectsAreValid)
+		{
+			Locations wayPoints = new Locations();
+			//ArrayList<Location> wayPoints = new ArrayList<Location>();
+
+			int i = 0;
+			while(i < routeArray.size())
+			{
+				wayPoints.addLocation(routeArray.get(i));
+			}
+				
+			System.out.println("Calculating route...");
+			System.out.println("Route calculation successful!\n");
+			return wayPoints;
+		}
+		else
+		{
+			System.out.println("Location Objects are invalid.\n");
+		}
+				
+		return null;
+	}
+
+	public Locations getLocations()
 		{
 			return this.path;
 		}
