6fbcb0d9662b812075486ba9d6a04f8478c9407a
vossca <mgerber0608@gmail.com>
2017-04-04 18:43:17 +0200

    Update FailedValidation.java

--- Navigation/src/main/java/com/gladius/navup/navigation/FailedValidation.java
+++ Navigation/src/main/java/com/gladius/navup/navigation/FailedValidation.java
@@ -5,17 +5,35 @@ import java.util.*;
 import java.lang.Object;
 import java.lang.Throwable;
 
+/**
+ * Custom exception class for exception that will be raised if general route creation fails
+ * Example use case: Cannot connect to database to retrieve user preferences
+ * @author  Mia Gerber
+ */
 public class FailedValidation extends NavigationExceptions {
 	public FailedValidation() {}
 
+	/**
+	* Constructor 
+	* @param 	mesg	object of type String to be output as a description of exception that is raised
+	*/
 	public FailedValidation(String mesg) {
 		super(mesg);
 	}
 
+	/**
+	* Constructor
+	* @param 	reason	object of type Throwable to be thrown if exception is raised
+	*/
 	public FailedValidation(Throwable reason) {
 		super(reason);
 	}
 
+	/**
+	* Constructor
+	* @param	mesg	object of type String to be output as a description of exception that is raised
+	* @param 	reason	object of type Throwable to be thrown if exception is raised
+	*/
 	public FailedValidation(String mesg, Throwable reason) {
 		super(mesg,reason);
 	}
