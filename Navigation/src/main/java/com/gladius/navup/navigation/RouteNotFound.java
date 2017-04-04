3a9da81c10be27279a604aa845f7e7dd37d183f1
vossca <mgerber0608@gmail.com>
2017-04-04 18:35:50 +0200

    Update RouteNotFound.java

--- Navigation/src/main/java/com/gladius/navup/navigation/RouteNotFound.java
+++ Navigation/src/main/java/com/gladius/navup/navigation/RouteNotFound.java
@@ -5,17 +5,35 @@ import java.util.*;
 import java.lang.Object;
 import java.lang.Throwable;
 
+/**
+ * Custom exception class for exception that will be raised if a route for given constraints cannot be found
+ * Example use case: Requesting a path for wheelchair access that has none
+ * @author  Mia Gerber
+ */
 public class RouteNotFound extends NavigationExceptions {
 	public RouteNotFound() {}
 
+	/**
+	* Constructor 
+	* @param 	mesg	object of type String to be output as a description of exception that is raised
+	*/
 	public RouteNotFound(String mesg) {
 		super(mesg);
 	}
 
+	/**
+	* Constructor
+	* @param 	reason	object of type Throwable to be thrown if exception is raised
+	*/
 	public RouteNotFound(Throwable reason) {
 		super(reason);
 	}
 
+	/**
+	* Constructor
+	* @param	mesg	object of type String to be output as a description of exception that is raised
+	* @param 	reason	object of type Throwable to be thrown if exception is raised
+	*/
 	public RouteNotFound(String mesg, Throwable reason) {
 		super(mesg,reason);
 	}
