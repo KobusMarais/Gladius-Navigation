b7223b40d224b88f9e352269f8883343427daa3f
vossca <mgerber0608@gmail.com>
2017-04-04 18:37:28 +0200

    Update IncompleteInformation.java

--- Navigation/src/main/java/com/gladius/navup/navigation/IncompleteInformation.java
+++ Navigation/src/main/java/com/gladius/navup/navigation/IncompleteInformation.java
@@ -5,17 +5,35 @@ import java.util.*;
 import java.lang.Object;
 import java.lang.Throwable;
 
+/**
+ * Custom exception class to be raised if information received from other modules is incomplete
+ * Example use case: If we request a location but only receive latitude not longitude
+ * @author  Mia Gerber
+ */
 public class IncompleteInformation extends NavigationExceptions {
 	public IncompleteInformation() {}
 
+	/**
+	* Constructor 
+	* @param 	mesg	object of type String to be output as a description of exception that is raised
+	*/
 	public IncompleteInformation(String mesg) {
 		super(mesg);
 	}
 
+	/**
+	* Constructor
+	* @param 	reason	object of type Throwable to be thrown if exception is raised
+	*/
 	public IncompleteInformation(Throwable reason) {
 		super(reason);
 	}
 
+	/**
+	* Constructor
+	* @param	mesg	object of type String to be output as a description of exception that is raised
+	* @param 	reason	object of type Throwable to be thrown if exception is raised
+	*/
 	public IncompleteInformation(String mesg, Throwable reason) {
 		super(mesg,reason);
 	}
