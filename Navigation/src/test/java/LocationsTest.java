1f176c74d4410132d680fb3a1f4e541d363f69ae
Darren Adams <shiznet101.darren@gmail.com>
2017-04-04 19:03:23 +0200

    Add test files on behalf of Victor

---
+++ Navigation/src/test/java/LocationsTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+import com.gladius.navup.navigation.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by victor on 2017/03/22.
+ */
+class LocationsTest {
+    @Test
+    void addLocation() {
+//        GPSObject gpsObject = new GPSObject("start");
+//        Location location = new Location("start", "1", gpsObject);
+//        addLocation(Location location);
+    }
+
+    /**
+     *
+     * This test method sends Location object 'inLoc' for modification via the modifyLocation() method from the locations class.
+     * If successful, the method will return a new modified Location object.
+     * If unsuccessful, the method will return null.
+     */
+
+    @Test
+    void modifyLocation() {
+        Locations locations = new Locations();
+        GPSObject obj = new GPSObject("obj");
+        Location inLoc = new Location("start", "1", obj);
+        assertNotNull(locations.modifyLocation(inLoc, "location", "1", obj));
+    }
+
+    @Test
+    void removeLocation() {
+//        Location inLoc = new Location("start", "1", obj);
+//        removeLocation(inLoc);
+    }
+
+}
